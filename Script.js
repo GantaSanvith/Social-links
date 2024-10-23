48eca639fe9750b959e34a8bb6ab02056c9037e0
GantaSanvith <129515433+GantaSanvith@users.noreply.github.com>
2024-10-23 12:38:19 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,18 @@
+function redirectToGit(){
+    window.location.href="https://github.com/GantaSanvith";
+}
+
+function redirectToLinkedIn(){
+    window.location.href="https://www.linkedin.com/in/sanvith-ganta-24b299292/";
+}
+
+function redirectToinsta(){
+    window.location.href="https://www.instagram.com/sanvithganta/";
+}
+
+function redirectToTwitter(){
+    window.location.href="https://x.com/?lang=en";
+}
+function redirectToFm(){
+    window.location.href="https://www.frontendmentor.io/challenges/social-links-profile-UG32l9m6dQ";
+}
